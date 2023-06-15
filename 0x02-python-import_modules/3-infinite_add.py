2110afbb6947b36538cc8f2419b724a7eacfd3e9
Mary-Afrika <marygahar@gmail.com>
2023-06-15 08:11:19 +0000

    ALX

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    ac = len(sys.argv) - 1
-    sum = 0
-    for i in range(0, ac):
-        sum += int(sys.argv[i + 1])
-    print('{0}'.format(sum))
