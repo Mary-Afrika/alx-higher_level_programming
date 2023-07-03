00aafc2a88a0887908b6c1ef4d02b4a95a59b5df
Mary-Afrika <marygahar@gmail.com>
2023-07-03 08:51:13 -0700

    ALX

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    ac = len(sys.argv) - 1
+    if ac != 3:
+        print('Usage: {} <a> <operator> <b>'.format(sys.argv[0]))
+        exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator == '+':
+        res = add(a, b)
+    elif operator == '-':
+        res = sub(a, b)
+    elif operator == '*':
+        res = mul(a, b)
+    elif operator == '/':
+        res = div(a, b)
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    print('{0} {1} {2} = {3}'.format(a, operator, b, res))
