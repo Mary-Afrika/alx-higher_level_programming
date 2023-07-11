7bcdd65ffa878993a3c5c842084f1a76e536732a
Mary-Afrika <marygahar@gmail.com>
2023-07-10 20:49:39 -0700

    ALX

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Lookup function documentation"""
-
-
-def lookup(obj):
-    """function that returns the list of all available attributes and methods \
-     of an object"""
-
-    return dir(obj)
