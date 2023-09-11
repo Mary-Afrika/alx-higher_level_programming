6105f44e5fef3b4e11bc7c938b58958fc5be0d03
Mary-Afrika <marygahar@gmail.com>
2023-09-11 10:59:12 -0700

    ALX

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-
-'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
